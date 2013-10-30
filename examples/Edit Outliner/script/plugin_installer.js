1a29ed8bf630006d63af77672d51698287e47744
Jennifer Winer <jenn.winer@gmail.com>
2013-10-30 14:42:58 -0400

    Save for one bug now 4.0 Compliant.

--- examples/Edit Outliner/script/plugin_installer.js
+++ examples/Edit Outliner/script/plugin_installer.js
@@ -3,13 +3,73 @@
  * FIXME - needs a liscence
  *******************************************************************************/
 
-/*global Tautologistics eclipse self window*/
+/*global Tautologistics orion self window*/
 
 window.onload = function(){
-
+	function parseHeadings(content){
+		var outline = [];
+	    var lines, line;
+	    var match;
+	    
+	    lines = content.split(/\r?\n/);
+	    for (var i=0; i < lines.length; i++) {
+	        line = lines[i];
+	        match = /^=\s*(.+?)\s*=$/.exec(line);
+	      
+	        if (match) {
+	        	// remove the = tags from the header
+	        	
+	        	// FIXME - change so that it only grabs the name of the header (without the ==)
+	        	// FIXME - modify to accomidate nesting of headers (to give an example of the header tags)
+	            outline.push({
+	                label: match[1],
+	                line: i+1  // lines are numbered from 1
+	       	    });
+	        }
+	    }
+		return outline;
+	}
+	
 	// The functionality of your plugin
 	var outlineService = {
 		/**
+		 * Orion 4.0 Compliant
+		 * @param {ObjectReference} editorContext allows two way communication 
+		 * 				between the plugin and the Orion Editor
+		 * 				- Please see "Editor Context Informaiton.txt" for more 
+		 * 
+		 * @param {String} options.contentType the The Content Type ID of the file
+		 * 				being edited. Please see the file "List of Content Types.txt"
+		 * 				for more informaiton 
+		 * 
+		 * @return {Array} an array consisting of the elements that compose an outline
+		 * 
+		 * Each element of the Array can contain the following properties:
+		 * =================================================================
+		 * label 		= The text that will be shown in the outline
+		 * 
+		 * [className] 	= A space-separated list of CSS class names to be applied to the label
+		 * [children] 	= An Array of nested elements
+		 * 
+		 * 
+		 * Please only use one of the following:
+		 * --------------------------------------
+		 * [line] 		= The line in the file this label will link to. For finer control 
+		 * 					please see the variables column, start, end, text inside the
+		 * 					 orion.util.hashFromPosition() documentation on orion wiki.
+		 * [href]		= The URL that this label will link to
+		 */
+		computeOutline: function(editorContext, options){
+			// FIXME - make this work
+		//return editorContext.getText().then(parseHeadings);
+		
+			return [{label:"4.0 Compliance: The method editorContext.getText() does not appear to be working yet ..."}];
+		//	return parseHeadings("===Heading===\n more infor\n== Heading 2 ===");
+		},
+		
+		
+		/**
+		 * Orion 3.0 Compliant
 		 * @param {String} contents The contents of the file being edited
 		 * @param {String} title The path and the filename of the file being edited
 		 * @return {Array} an array consisting of the elements that compose an outline
@@ -30,29 +90,7 @@ window.onload = function(){
 		 * [href]		= The URL that this label will link to
 		 */
 		getOutline: function(contents, title){
-			var outline = [];
-		    var lines, line;
-		    var match;
-		    var heading;
-		    
-		    lines = contents.split(/\r?\n/);
-		    for (var i=0; i < lines.length; i++) {
-		        line = lines[i];
-		        match = /^=\s*(.+?)\s*=$/.exec(line);
-		      
-		        if (match) {
-		        	// remove the = tags from the header
-		        	
-		        	// FIXME - change so that it only grabs the name of the header (without the ==)
-		        	// FIXME - modify to accomidate nesting of headers (to give an example of the header tags)
-		            outline.push({
-		                label: match[1],
-		                line: i+1  // lines are numbered from 1
-		       	    });
-                }
-		    }
-		    
-			return outline;
+			return parseHeadings(contents);
 		}
 	};
 
@@ -76,10 +114,15 @@ window.onload = function(){
 		 */
 		id: "orion.edit.outliner.mediawiki headings",
 		
-		// The name of your plugin
+		/**
+		 * The name of your plugin
+		 */
 		name: "Mediawiki Headings Plugin",
 		
-		// The list of file types that your plugin will work under
+		/**
+		 * The list of file types that your plugin will work under
+		 * 		please see the file "List of Content Types.txt" for more informaiton 
+		 */
         contentType: ["text/plain"]
 	};
 	
@@ -88,7 +131,7 @@ window.onload = function(){
 	 * Register the plugin with Orion
 	 * *** DO NOT TOUCH BELOW THIS LINE ***
 	 */
-	var provider = new eclipse.PluginProvider(headers);
+	var provider = new orion.PluginProvider(headers);
 	provider.registerServiceProvider("orion.edit.outliner", outlineService, serviceProperties);
 	provider.connect();
 };
